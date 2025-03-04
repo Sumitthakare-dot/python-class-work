c98354633d6b32a2d9f47a72edee1519bada8cc8
SUMIT THAKARE <sumitthakare122@gmail.com>
2025-03-04 08:29:49 +0530

    Add files via upload

---
+++ WAP to ask user to enter names of their 3 favorite movies & store them in a list.py
@@ -0,0 +1,10 @@
+movie = []
+mov1= input("Enetr first movie:")
+mov2= input("Enetr second movie:")
+mov3= input("Enetr third movie:")
+
+movie.append(mov1)
+movie.append(mov2)
+movie.append(mov3)
+
+print(movie)
---
+++ WAP to count the number of students with the A grade in the following tuple.py
@@ -0,0 +1,3 @@
+# ["c","d","a","a","b","b","a"]
+grade = ("c","d","a","a","b","b","a")
+print(grade.count("a"))
